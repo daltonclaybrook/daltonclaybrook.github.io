bb710ab9f4afed1a5308bd72d3e9953ad8ee57e7
Dalton Claybrook <daltonclaybrook@gmail.com>
2018-01-20 11:41:30 -0600

    Moved resources folder to root and created Header component

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import { ButtonContainer } from './components/button-container';
 import { FormComponent } from './components/form-component';
+import { Header } from './components/header';
 
 class App extends React.Component<{}, {}> {
     render() {
         return (
             <div>
-                <h1>This is the app component.</h1>
+                <Header title="Ball Fondlers" />
                 <FormComponent formName="Controlled Input Example">
                     <p>
                         Changing the color of the buttons in the component below
---
+++ src/components/header.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react'
+
+interface Props {
+    title: string;
+}
+
+class Header extends React.Component<Props, {}> {    
+    render() {
+        const { title } = this.props;
+        return (
+            <h1>
+                {title.toUpperCase()}
+            </h1>
+        )
+    }
+}
+
+export { Header };
