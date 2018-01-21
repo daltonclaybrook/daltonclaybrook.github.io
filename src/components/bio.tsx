514668dcb085705f931f40a93a6c985465f8464a
Dalton Claybrook <daltonclaybrook@gmail.com>
2018-01-20 18:40:08 -0600

    Now loading bio from markdown

--- src/components/bio.tsx
+++ src/components/bio.tsx
@@ -1,12 +1,32 @@
 import * as React from 'react'
+import * as Showdown from 'showdown'
+import * as API from '../business/api'
+import { Section } from './section'
 
-class Bio extends React.Component<{}, {}> {
+interface State {
+    bioHTML: string
+}
+
+class Bio extends React.Component<{}, State> {
+    constructor(props: {}) {
+        super(props)
+        this.state = { bioHTML: 'Loading...' }
+    }
+
     render() {
+        const innerHTML = { __html: this.state.bioHTML }
         return (
-            <p>
-                This is my bio. I hope you like it.
-            </p>
+            <Section>
+                <div dangerouslySetInnerHTML={innerHTML} />
+            </Section>
         )
+    }
+
+    async componentDidMount() {
+        const bioMarkdown = await API.fetchBio()
+        const converter = new Showdown.Converter()
+        const bioHTML = converter.makeHtml(bioMarkdown)
+        this.setState({ bioHTML })
     }
 }
 
